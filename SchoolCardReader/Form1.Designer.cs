9906ef50feafceb9e6d616b7c68f584d253ca163
Sosov Nikolay <djwirtuoz@gmail.com>
2023-01-30 20:20:06 +0300

    Добавлено разделение по типам карт

---
+++ SchoolCardReader/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SchoolCardReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.fam_textB = new System.Windows.Forms.TextBox();
+            this.num_textB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.port_comboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(12, 71);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(150, 23);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Старт";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // fam_textB
+            // 
+            this.fam_textB.Location = new System.Drawing.Point(12, 31);
+            this.fam_textB.Name = "fam_textB";
+            this.fam_textB.Size = new System.Drawing.Size(44, 20);
+            this.fam_textB.TabIndex = 3;
+            // 
+            // num_textB
+            // 
+            this.num_textB.Location = new System.Drawing.Point(62, 31);
+            this.num_textB.Name = "num_textB";
+            this.num_textB.Size = new System.Drawing.Size(100, 20);
+            this.num_textB.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Серия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Номер";
+            // 
+            // port_comboBox
+            // 
+            this.port_comboBox.FormattingEnabled = true;
+            this.port_comboBox.Location = new System.Drawing.Point(239, 30);
+            this.port_comboBox.Name = "port_comboBox";
+            this.port_comboBox.Size = new System.Drawing.Size(147, 21);
+            this.port_comboBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(236, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Порт";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(236, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 23);
+            this.label5.TabIndex = 9;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.TextChanged += new System.EventHandler(this.label5_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 113);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.port_comboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.num_textB);
+            this.Controls.Add(this.fam_textB);
+            this.Controls.Add(this.btn1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Считывание карт";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox fam_textB;
+        private System.Windows.Forms.TextBox num_textB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox port_comboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SchoolCardReader/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
+
+namespace SchoolCardReader
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port = new SerialPort();
+        string selectedPort;
+        bool isConnected = false;
+        string command_pack;
+        string command;
+        string all_num; //HEX all numm
+        string family;  //family numm DEC
+        string number;  //number numm DEC
+        public Form1()
+        {
+            InitializeComponent();
+
+            port.DataReceived += new SerialDataReceivedEventHandler(OnDataReceived);    //подключение события получения данных
+        }
+
+        private void OnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            bool _continue = true;
+            if (port.IsOpen)
+            {
+                while (_continue)
+                {
+                    try
+                    {
+                        string message = port.ReadLine();
+                        command_pack = message;
+                    }
+                    catch (TimeoutException) { }
+                    finally
+                    {
+                        _continue = false;
+                        ConvertData();
+                    }
+                }
+            }
+            port.Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (port.IsOpen)
+            {
+                e.Cancel = true; //cancel the fom closing
+                Thread CloseDown = new Thread(new ThreadStart(CloseSerialOnExit)); //close port in new thread to avoid hang
+                CloseDown.Start(); //close port in new thread to avoid hang
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Search_dev();
+        }
+
+        private void Search_dev()
+        {
+            btn1.Enabled = false;
+            try
+            {
+                port_comboBox.Items.Clear();
+                // Получаем список COM портов доступных в системе
+                string[] portnames = SerialPort.GetPortNames();
+                // Проверяем есть ли доступные
+                if (portnames.Length == 0)
+                {
+                    label5.Text = "Устройство не найдено";
+                    btn1.Enabled = false;
+                }
+                else
+                {
+                    for (int x = 0; x < portnames.Length; x++)
+                    {
+                        //добавляем доступные COM порты в список           
+                        port_comboBox.Items.Add(portnames[x]);
+                        port_comboBox.SelectedItem = portnames[x];
+                        selectedPort = port_comboBox.GetItemText(port_comboBox.SelectedItem);
+                        port.PortName = selectedPort;
+                        port.BaudRate = 115200;
+
+                        if (portnames[x] != null)
+                        {
+                            btn1.Enabled = true;
+                            port_comboBox.SelectedItem = portnames[x];
+                            label5.Text = "Выберите порт";
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!isConnected)
+                {
+                    isConnected = true;
+                    port.Open();
+                    btn1.Text = "Стоп";
+                    label5.Text = "Поднесите карту";
+                }
+                else
+                {
+                    isConnected = false;
+                    port.Close();
+                    btn1.Text = "Старт";
+                    label5.Text = "Считывание остановлено";
+                }
+            }
+            catch { label5.Text = "Порт недоступен"; }
+        }
+
+        public void ConvertData()
+        {
+            if (command_pack != null && command_pack.Length > 0 && command_pack != "\r")
+            {
+                string[] parts = command_pack.Split('-');
+                command = parts[0];
+                all_num = parts[1];
+
+                if (command == "125")
+                {
+                    all_num = all_num.Trim();
+                    if (all_num.Length == 5) { all_num = "0" + all_num; }
+                    if (all_num.Length == 4) { all_num = "00" + all_num; }
+
+                    all_num = all_num.Substring(0, 6);
+                    family = all_num.Substring(0, 2);
+                    int family_DEC = Convert.ToInt32(family, 16);
+                    fam_textB.BeginInvoke((Action)delegate () { fam_textB.Text = family_DEC.ToString(); ; });
+
+                    number = all_num.Substring(2, 4);
+                    int number_DEC = Convert.ToInt32(number, 16);
+                    num_textB.BeginInvoke((Action)delegate () { num_textB.Text = number_DEC.ToString(); ; });
+
+                    port.Close();
+                    isConnected = false;
+                    btn1.BeginInvoke((Action)delegate () { btn1.Text = "Старт"; ; });
+                    label5.BeginInvoke((Action)delegate () { label5.Text = "Считывание остановлено"; ; });
+                }
+
+                if(command == "135")
+                {
+                    all_num = all_num.Substring(0, 8);
+                    all_num = all_num.Trim();
+
+                    string[] hex_chanks = all_num.Split(',');
+                    Array.Reverse(hex_chanks);
+                    string numm = "";
+
+                    for (int i = 0; i < hex_chanks.Length; i++)
+                    {
+                        numm += hex_chanks[i];
+                    }
+
+                    if (numm.Length == 5) { numm = "0" + numm; }
+                    if (numm.Length == 4) { numm = "00" + numm; }
+
+                    family = numm.Substring(0, 2);
+                    int family_DEC = Convert.ToInt32(family, 16);
+                    fam_textB.BeginInvoke((Action)delegate () { fam_textB.Text = family_DEC.ToString(); ; });
+
+                    number = numm.Substring(2, 4);
+                    int number_DEC = Convert.ToInt32(number, 16);
+                    num_textB.BeginInvoke((Action)delegate () { num_textB.Text = number_DEC.ToString(); ; });
+
+                    port.Close();
+                    isConnected = false;
+                    btn1.BeginInvoke((Action)delegate () { btn1.Text = "Старт"; ; });
+                    label5.BeginInvoke((Action)delegate () { label5.Text = "Считывание остановлено"; ; });
+                }
+            }
+        }
+
+        private void label5_TextChanged(object sender, EventArgs e)
+        {
+            if(label5.Text == "Поднесите карту")
+            {
+                label5.ForeColor = Color.Green;
+            }
+
+            if (label5.Text == "Считывание остановлено")
+            {
+                label5.ForeColor = Color.Red;
+            }
+        }
+
+        private void CloseSerialOnExit()
+        {
+            try
+            {
+                port.Close(); //close the serial port
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); //catch any serial port closing error messages
+            }
+            this.Invoke(new EventHandler(NowClose)); //now close back in the main thread
+        }
+
+        private void NowClose(object sender, EventArgs e)
+        {
+            this.Close(); //now close the form
+        }
+    }
+}
